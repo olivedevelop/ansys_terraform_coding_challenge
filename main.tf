808d8d93f94469c5c1fc278e6816232a48d01e33
olivedevelop <otuch@hawk.iit.edu>
2024-02-03 19:35:43 +0100

    migrating to a module format

--- main.tf
+++ main.tf
@@ -1,34 +1,7 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "MyVPC4Ansys"
-  }
-}
-
-resource "aws_subnet" "public_subnet" {
-  count = 3
-
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.${count.index + 1}.0/24"
-  availability_zone       = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "PublicSubnet-${count.index + 1}"
-  }
-}
-
-resource "aws_subnet" "private_subnet" {
-  count = 3
-
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.${count.index + 4}.0/24"
-  availability_zone       = element(var.availability_zones, count.index)
-
-  tags = {
-    Name = "PrivateSubnet-${count.index + 1}"
-  }
+module "my_vpc" {
+  source = "./modules/vpc_module"
+  
+  aws_region          = var.aws_region
+  vpc_cidr_block      = var.vpc_cidr_block
+  availability_zones  = var.availability_zones
 }
---
+++ modules/vpc_module/vpc_module.tf
@@ -0,0 +1,34 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block          = var.vpc_cidr_block
+  enable_dns_support  = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MyVPC4Ansys"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = 3
+
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.${count.index + 1}.0/24"
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count = 3
+
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.${count.index + 4}.0/24"
+  availability_zone       = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "PrivateSubnet-${count.index + 1}"
+  }
+}
