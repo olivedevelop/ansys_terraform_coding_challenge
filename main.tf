ac4cc4a525338126e0f604a6558960da4830e00c
olivedevelop <otuch@hawk.iit.edu>
2024-02-03 19:12:33 +0100

    adding the subnets

--- main.tf
+++ main.tf
@@ -7,3 +7,28 @@ resource "aws_vpc" "my_vpc" {
     Name = "MyVPC4Ansys"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  count = 3
+
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.${count.index + 1}.0/24"
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count = 3
+
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.${count.index + 4}.0/24"
+  availability_zone       = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "PrivateSubnet-${count.index + 1}"
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
 aws_region     = "us-east-1"
 vpc_cidr_block = "10.0.0.0/16"
+availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
