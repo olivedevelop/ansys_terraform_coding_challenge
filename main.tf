ee61f4acbe121e21628f25549d845c4c96092de3
olivedevelop <otuch@hawk.iit.edu>
2024-02-03 18:31:06 +0100

    creating the vpc

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MyVPC4Ansys"
+  }
+}
