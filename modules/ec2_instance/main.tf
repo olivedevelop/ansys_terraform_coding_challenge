b467cbcfc2618c270863611e2a1322ff20b6d3ff
olivedevelop <otuch@hawk.iit.edu>
2024-02-05 13:09:38 +0100

    improving ingress rule

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -18,8 +18,18 @@ provisioner "remote-exec" {
 
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
-  description = "Allow inbound HTTP traffic"
+  description = "Allow inbound HTTP traffic based on user-defined ingress rules"
   vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
 
   ingress {
     from_port   = 80
