8e214ea17eeaa621b502b9e561413e82c7600754
olivedevelop <otuch@hawk.iit.edu>
2024-02-05 13:19:16 +0100

    adding igw and route table to allow public subnets internet access  while blocking private subnets

--- modules/vpc_module/main.tf
+++ modules/vpc_module/main.tf
@@ -33,10 +33,43 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(var.availability_zones)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = length(var.availability_zones)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
 output "vpc_id" {
   value = aws_vpc.my_vpc.id
 }
 
 output "public_subnets" {
   value = aws_subnet.public_subnet[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private_subnet[*].id
 }
